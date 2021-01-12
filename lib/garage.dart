0efcb221f46d7494e61fcd936ac11b6f12098877
yohann <yohann.catherine@outlook.fr>
2021-01-12 10:16:42 +0100

    Tentative

--- lib/garage.dart
+++ lib/garage.dart
@@ -2,22 +2,31 @@ import 'voiture.dart';
 
 class CabinetComptable {
   final clients = <Object, int>{};
+  int _tresorerie;
 
   void addClient(Object client) {
     clients[client] = null;
   }
 
   String toString() {
-    return "clients : $clients";
+    return 'clients : $clients';
   }
+  
+  void addMoney(int s) {
+    _tresorerie = _tresorerie + s;
+  }
+
+  int get tresorerie => _tresorerie;
 }
 
 class Garage {
   final _objetsAReparer = <Vehicule>[];
+  var cabinet = CabinetComptable();
   Garage();
 
   void nouvellePriseCharge(Vehicule vehicule) {
     _objetsAReparer.add(vehicule);
+    cabinet.addMoney(15);
   }
 
   @override
